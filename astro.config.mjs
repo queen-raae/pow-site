9d5f972b9957d9713249453cc031865b62d11e10
Ola <ola@olavea.com>
2024-09-14 13:55:03 +0200

    yo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
-import netlify from "@astrojs/netlify";
-
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pow-site-astro.netlify.app/",
-  integrations: [tailwind(), sitemap()],
-  output: "hybrid",
-  adapter: netlify(),
+  site: "https://astro-simple-starter.netlify.app/",
+  integrations: [tailwind(), sitemap()]
 });
