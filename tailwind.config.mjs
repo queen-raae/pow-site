f50b86309169e5631bb5a57fc3c8dc0c1ae38348
Ola <ola@olavea.com>
2024-12-19 13:55:40 +0100

    I got the layout to work added         <br /> and an arrow 🏹  #29

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      listStyleImage: {
+        arrow: 'url("/public/arrow.png")',
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
